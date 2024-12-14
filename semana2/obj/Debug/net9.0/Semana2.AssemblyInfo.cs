7ddf57bcf49ee1ef549fc379545778b6644a9af6
Dinatang <dm.tanguilav@uea.edu.ec>
2024-12-14 17:27:49 -0500

    actualizar

--- semana2/obj/Debug/net9.0/Semana2.AssemblyInfo.cs
+++ semana2/obj/Debug/net9.0/Semana2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Semana2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f5600586e5d3a472c8578d7e6beab7c122f3bcf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d21030569309a949550465bc71bdb88e9fce0668")]
 [assembly: System.Reflection.AssemblyProductAttribute("Semana2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Semana2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
