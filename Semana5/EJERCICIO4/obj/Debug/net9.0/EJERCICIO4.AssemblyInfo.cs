aa282304d8fc0d3c6402ec67dee89721327686d1
Dinatang <dm.tanguilav@uea.edu.ec>
2025-01-10 15:13:54 -0500

    actualizar

--- Semana5/EJERCICIO4/obj/Debug/net9.0/EJERCICIO4.AssemblyInfo.cs
+++ Semana5/EJERCICIO4/obj/Debug/net9.0/EJERCICIO4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EJERCICIO4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97e1ca70da09993e5632dd5faa7c1c9be8a116c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38420bef2d662b41d203271ef2caaab58f4e3f3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("EJERCICIO4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EJERCICIO4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
