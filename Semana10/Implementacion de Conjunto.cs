9f611a0decb9fe0a747873d5e1809f64b36cb066
Dinatang <dm.tanguilav@uea.edu.ec>
2025-02-20 17:30:51 -0500

    Se corrigio la coma

---
+++ Semana10/Implementacion de Conjunto.cs
@@ -0,0 +1,39 @@
+﻿
+
+class Program
+{
+    static void Main()
+    {
+        // Crear un conjunto ficticio de 500 ciudadanos
+        HashSet<int> ciudadanos = new HashSet<int>(Enumerable.Range(1, 500));
+
+        // Crear un conjunto ficticio de 75 ciudadanos vacunados con Pfizer
+        HashSet<int> vacunadosPfizer = new HashSet<int>(Enumerable.Range(1, 75));
+
+        // Crear un conjunto ficticio de 75 ciudadanos vacunados con AstraZeneca
+        HashSet<int> vacunadosAstraZeneca = new HashSet<int>(Enumerable.Range(76, 75));
+
+        // Ciudadanos que no se han vacunado
+        HashSet<int> noVacunados = new HashSet<int>(ciudadanos);
+        noVacunados.ExceptWith(vacunadosPfizer);
+        noVacunados.ExceptWith(vacunadosAstraZeneca);
+
+        // Ciudadanos que han recibido las dos vacunas (intersección de los dos conjuntos)
+        HashSet<int> vacunadosAmbas = new HashSet<int>(vacunadosPfizer);
+        vacunadosAmbas.IntersectWith(vacunadosAstraZeneca);
+
+        // Ciudadanos que solamente han recibido la vacuna de Pfizer
+        HashSet<int> soloPfizer = new HashSet<int>(vacunadosPfizer);
+        soloPfizer.ExceptWith(vacunadosAstraZeneca);
+
+        // Ciudadanos que solamente han recibido la vacuna de AstraZeneca
+        HashSet<int> soloAstraZeneca = new HashSet<int>(vacunadosAstraZeneca);
+        soloAstraZeneca.ExceptWith(vacunadosPfizer);
+
+        // Mostrar resultados
+        Console.WriteLine("Ciudadanos no vacunados: " + noVacunados.Count);
+        Console.WriteLine("Ciudadanos vacunados con ambas vacunas: " + vacunadosAmbas.Count);
+        Console.WriteLine("Ciudadanos vacunados solo con Pfizer: " + soloPfizer.Count);
+        Console.WriteLine("Ciudadanos vacunados solo con AstraZeneca: " + soloAstraZeneca.Count);
+    }
+}
--- Semana7/obj/Debug/net9.0/Semana7.AssemblyInfo.cs
+++ Semana7/obj/Debug/net9.0/Semana7.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Semana7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c8a0f29b387bc9fa5855d2beac56d5c28d75509")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed9f49fdae0208e8b7d21fb90e0c4ac60c22aff7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Semana7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Semana7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
