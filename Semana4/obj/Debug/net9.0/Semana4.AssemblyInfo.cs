5277a9969aeb38b69aaa8457fbf82db40f932590
Dinatang <dm.tanguilav@uea.edu.ec>
2025-02-20 17:39:29 -0500

    Se corrigio el  codigo

--- Semana4/obj/Debug/net9.0/Semana4.AssemblyInfo.cs
+++ Semana4/obj/Debug/net9.0/Semana4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Semana4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f611a0decb9fe0a747873d5e1809f64b36cb066")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b3b54b0267621aaed81f3a03f0a7817d817b316")]
 [assembly: System.Reflection.AssemblyProductAttribute("Semana4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Semana4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
